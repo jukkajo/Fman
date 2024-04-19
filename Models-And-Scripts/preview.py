ed2fc28ea64fb589d913b1ff583fdd918d31808a
jukkajo <76208694+jukkajo@users.noreply.github.com>
2024-04-19 12:39:49 +0300

    Add files via upload

---
+++ Models-And-Scripts/preview.py
@@ -0,0 +1,37 @@
+from pypylon import pylon
+import cv2
+
+# Loading model
+
+# conecting to the first available camera
+camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+
+# Grabing Continusely (video) with minimal delay
+camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
+converter = pylon.ImageFormatConverter()
+
+# converting to opencv bgr format
+converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+
+while camera.IsGrabbing():
+    grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+
+    if grabResult.GrabSucceeded():
+        # Access the image data
+        image = converter.Convert(grabResult)
+        img = image.GetArray()
+
+        # Predict
+        
+        # Exracting bbox coords and class prediction  
+
+        # Displaying
+        cv2.namedWindow('title', cv2.WINDOW_NORMAL)
+        cv2.imshow('title', img)
+        k = cv2.waitKey(1)
+        if k == 27:
+            break
+
+    grabResult.Release()
+camera.Close()
